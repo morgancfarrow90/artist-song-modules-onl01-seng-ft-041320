b456e3b3874f1ba76985249d2b203daf1384d505
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-02 13:16:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,7 +10,7 @@ class Artist
   
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
-    include Paramable::InstanceMethods
+  include Paramable::InstanceMethods
 
 
   @@artists = []
